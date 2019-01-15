989c34ebf6001d1cddbbc094e3770a7e205013cd
Jacek Laskowski <jacek@japila.pl>
2019-01-15 20:26:42 +0100

    Comparing KStream and KTable

---
+++ src/main/scala/KStreamApp.scala
@@ -0,0 +1,49 @@
+
+object KStreamApp extends App {
+
+  // Boilerplate (is supposed to make your dev life easier :))
+  // Bartek says: "Coś za coś" :)
+  import org.apache.kafka.streams.scala._
+  import Serdes._
+  import ImplicitConversions._
+
+  // Step 1. Create Topology
+  // ==> Streams DSL
+  // Processor API
+  val builder = new StreamsBuilder
+  import org.apache.kafka.streams.kstream.Printed
+  val console = Printed
+    .toSysOut[String, String]
+    .withLabel("DEBUG")
+
+  val topic = "exercise1-input"
+  import org.apache.kafka.streams.Topology
+  val consumed = consumedFromSerde[String, String]
+    .withOffsetResetPolicy(Topology.AutoOffsetReset.LATEST)
+  builder
+    .stream[String, String](topic)(consumed)
+    .peek((k,v) => println(s"(peek) $k -> $v"))
+    .print(console)
+
+  // Will this work?
+  // No!
+  // Invalid topology: Topic exercise1-input has already been registered by another source.
+  // You cannot use the same input
+//  builder.table[String, String](topic)
+//    .toStream
+//    .print(console)
+
+  val topology = builder.build()
+  println(topology.describe())
+
+  // Step 2. Run the topology
+  val props = new java.util.Properties()
+  val appId = this.getClass.getName.replace("$", "")
+  import org.apache.kafka.streams.StreamsConfig
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, appId)
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, ":9092") // FIXME Use scopt for cmd args
+  import org.apache.kafka.streams.KafkaStreams
+  val ks = new KafkaStreams(topology, props)
+  ks.start()
+
+}
--- src/main/scala/KTableApp.scala
+++ src/main/scala/KTableApp.scala
@@ -1,31 +1,34 @@
-import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
-import org.apache.kafka.streams.kstream.Printed
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, Topology}
 
-object Exercise1App extends App {
+object KTableApp extends App {
 
-  // Boilerplate (is supposed to make your dev life easier :))
-  // Bartek says: "Coś za coś" :)
+  val appId = this.getClass.getName.replace("$", "")
+
   import org.apache.kafka.streams.scala._
-  import Serdes._
   import ImplicitConversions._
+  import Serdes._
 
   // Step 1. Create Topology
   // ==> Streams DSL
   // Processor API
   val builder = new StreamsBuilder
+  import org.apache.kafka.streams.kstream.Printed
   val console = Printed
     .toSysOut[String, String]
     .withLabel("DEBUG")
-  builder
-    .stream[String, String](topic = "exercise1-input")
-    .peek((k,v) => println(s"(peek) $k -> $v"))
+
+  val topic = "exercise1-input"
+  val consumed = consumedFromSerde[String, String]
+    .withOffsetResetPolicy(Topology.AutoOffsetReset.LATEST)
+  builder.table[String, String](topic)(consumed)
+    .toStream
     .print(console)
 
   val topology = builder.build()
+  println(topology.describe())
 
   // Step 2. Run the topology
   val props = new java.util.Properties()
-  val appId = this.getClass.getName.replace("$", "")
   props.put(StreamsConfig.APPLICATION_ID_CONFIG, appId)
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, ":9092") // FIXME Use scopt for cmd args
   val ks = new KafkaStreams(topology, props)
